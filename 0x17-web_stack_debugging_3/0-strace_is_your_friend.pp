fff663fd0ca4a23deb02dfe7a30370ad803f7864
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-11-02 11:15:20 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# find out why Apache is returning a 500 error, using strace
+
+exec { 'typo error in /var/www/html/wp-settings.php file name':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp'
